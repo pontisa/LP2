ed671e5360bca7c24a1beea9ae870c855ce61bdc
pontisa <99200684+pontisa@users.noreply.github.com>
2023-09-27 09:42:37 -0300

    Add files via upload

---
+++ Atividade5/Psalario/Psalario/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Psalario
+{
+    partial class Psalario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNomeFuncionario = new System.Windows.Forms.Label();
+            this.lblSalarioBruto = new System.Windows.Forms.Label();
+            this.lblNumeroFilhos = new System.Windows.Forms.Label();
+            this.btnVerificaDesconto = new System.Windows.Forms.Button();
+            this.lblAliquotaINSS = new System.Windows.Forms.Label();
+            this.lblAliquotaIRPF = new System.Windows.Forms.Label();
+            this.lblSalarioFamilia = new System.Windows.Forms.Label();
+            this.lblSalarioLiquido = new System.Windows.Forms.Label();
+            this.lblDescontoINSS = new System.Windows.Forms.Label();
+            this.lblDescontoIRPF = new System.Windows.Forms.Label();
+            this.txtNomeFuncionario = new System.Windows.Forms.TextBox();
+            this.mskbxSalarioBruto = new System.Windows.Forms.MaskedTextBox();
+            this.updwnNumeroFilhos = new System.Windows.Forms.NumericUpDown();
+            this.mskbxAliquotaINSS = new System.Windows.Forms.MaskedTextBox();
+            this.mskbxAliquotaIRPF = new System.Windows.Forms.MaskedTextBox();
+            this.mskbxSalarioFamilia = new System.Windows.Forms.MaskedTextBox();
+            this.mskbxSalarioLiquido = new System.Windows.Forms.MaskedTextBox();
+            this.mskbxDescontoINSS = new System.Windows.Forms.MaskedTextBox();
+            this.mskbxDescontoIRPF = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.updwnNumeroFilhos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNomeFuncionario
+            // 
+            this.lblNomeFuncionario.AutoSize = true;
+            this.lblNomeFuncionario.Location = new System.Drawing.Point(47, 37);
+            this.lblNomeFuncionario.Name = "lblNomeFuncionario";
+            this.lblNomeFuncionario.Size = new System.Drawing.Size(133, 20);
+            this.lblNomeFuncionario.TabIndex = 0;
+            this.lblNomeFuncionario.Text = "Nome funcionário";
+            // 
+            // lblSalarioBruto
+            // 
+            this.lblSalarioBruto.AutoSize = true;
+            this.lblSalarioBruto.Location = new System.Drawing.Point(47, 86);
+            this.lblSalarioBruto.Name = "lblSalarioBruto";
+            this.lblSalarioBruto.Size = new System.Drawing.Size(101, 20);
+            this.lblSalarioBruto.TabIndex = 1;
+            this.lblSalarioBruto.Text = "Salário Bruto";
+            // 
+            // lblNumeroFilhos
+            // 
+            this.lblNumeroFilhos.AutoSize = true;
+            this.lblNumeroFilhos.Location = new System.Drawing.Point(47, 136);
+            this.lblNumeroFilhos.Name = "lblNumeroFilhos";
+            this.lblNumeroFilhos.Size = new System.Drawing.Size(128, 20);
+            this.lblNumeroFilhos.TabIndex = 2;
+            this.lblNumeroFilhos.Text = "Número de filhos";
+            // 
+            // btnVerificaDesconto
+            // 
+            this.btnVerificaDesconto.Location = new System.Drawing.Point(168, 181);
+            this.btnVerificaDesconto.Name = "btnVerificaDesconto";
+            this.btnVerificaDesconto.Size = new System.Drawing.Size(217, 31);
+            this.btnVerificaDesconto.TabIndex = 3;
+            this.btnVerificaDesconto.Text = "Verifica Desconto";
+            this.btnVerificaDesconto.UseVisualStyleBackColor = true;
+            // 
+            // lblAliquotaINSS
+            // 
+            this.lblAliquotaINSS.AutoSize = true;
+            this.lblAliquotaINSS.Location = new System.Drawing.Point(48, 246);
+            this.lblAliquotaINSS.Name = "lblAliquotaINSS";
+            this.lblAliquotaINSS.Size = new System.Drawing.Size(109, 20);
+            this.lblAliquotaINSS.TabIndex = 4;
+            this.lblAliquotaINSS.Text = "Alíquota INSS";
+            // 
+            // lblAliquotaIRPF
+            // 
+            this.lblAliquotaIRPF.AutoSize = true;
+            this.lblAliquotaIRPF.Location = new System.Drawing.Point(48, 296);
+            this.lblAliquotaIRPF.Name = "lblAliquotaIRPF";
+            this.lblAliquotaIRPF.Size = new System.Drawing.Size(108, 20);
+            this.lblAliquotaIRPF.TabIndex = 5;
+            this.lblAliquotaIRPF.Text = "Alíquota IRPF";
+            // 
+            // lblSalarioFamilia
+            // 
+            this.lblSalarioFamilia.AutoSize = true;
+            this.lblSalarioFamilia.Location = new System.Drawing.Point(48, 342);
+            this.lblSalarioFamilia.Name = "lblSalarioFamilia";
+            this.lblSalarioFamilia.Size = new System.Drawing.Size(112, 20);
+            this.lblSalarioFamilia.TabIndex = 6;
+            this.lblSalarioFamilia.Text = "Salário Família";
+            // 
+            // lblSalarioLiquido
+            // 
+            this.lblSalarioLiquido.AutoSize = true;
+            this.lblSalarioLiquido.Location = new System.Drawing.Point(48, 388);
+            this.lblSalarioLiquido.Name = "lblSalarioLiquido";
+            this.lblSalarioLiquido.Size = new System.Drawing.Size(113, 20);
+            this.lblSalarioLiquido.TabIndex = 7;
+            this.lblSalarioLiquido.Text = "Salário Líquido";
+            // 
+            // lblDescontoINSS
+            // 
+            this.lblDescontoINSS.AutoSize = true;
+            this.lblDescontoINSS.Location = new System.Drawing.Point(445, 246);
+            this.lblDescontoINSS.Name = "lblDescontoINSS";
+            this.lblDescontoINSS.Size = new System.Drawing.Size(120, 20);
+            this.lblDescontoINSS.TabIndex = 8;
+            this.lblDescontoINSS.Text = "Desconto INSS";
+            // 
+            // lblDescontoIRPF
+            // 
+            this.lblDescontoIRPF.AutoSize = true;
+            this.lblDescontoIRPF.Location = new System.Drawing.Point(446, 296);
+            this.lblDescontoIRPF.Name = "lblDescontoIRPF";
+            this.lblDescontoIRPF.Size = new System.Drawing.Size(119, 20);
+            this.lblDescontoIRPF.TabIndex = 9;
+            this.lblDescontoIRPF.Text = "Desconto IRPF";
+            // 
+            // txtNomeFuncionario
+            // 
+            this.txtNomeFuncionario.Location = new System.Drawing.Point(195, 31);
+            this.txtNomeFuncionario.Name = "txtNomeFuncionario";
+            this.txtNomeFuncionario.Size = new System.Drawing.Size(248, 26);
+            this.txtNomeFuncionario.TabIndex = 10;
+            // 
+            // mskbxSalarioBruto
+            // 
+            this.mskbxSalarioBruto.Location = new System.Drawing.Point(195, 83);
+            this.mskbxSalarioBruto.Mask = "999990.00";
+            this.mskbxSalarioBruto.Name = "mskbxSalarioBruto";
+            this.mskbxSalarioBruto.Size = new System.Drawing.Size(248, 26);
+            this.mskbxSalarioBruto.TabIndex = 11;
+            // 
+            // updwnNumeroFilhos
+            // 
+            this.updwnNumeroFilhos.Location = new System.Drawing.Point(195, 130);
+            this.updwnNumeroFilhos.Name = "updwnNumeroFilhos";
+            this.updwnNumeroFilhos.Size = new System.Drawing.Size(139, 26);
+            this.updwnNumeroFilhos.TabIndex = 12;
+            // 
+            // mskbxAliquotaINSS
+            // 
+            this.mskbxAliquotaINSS.Location = new System.Drawing.Point(192, 243);
+            this.mskbxAliquotaINSS.Name = "mskbxAliquotaINSS";
+            this.mskbxAliquotaINSS.Size = new System.Drawing.Size(210, 26);
+            this.mskbxAliquotaINSS.TabIndex = 13;
+            // 
+            // mskbxAliquotaIRPF
+            // 
+            this.mskbxAliquotaIRPF.Location = new System.Drawing.Point(192, 293);
+            this.mskbxAliquotaIRPF.Name = "mskbxAliquotaIRPF";
+            this.mskbxAliquotaIRPF.Size = new System.Drawing.Size(210, 26);
+            this.mskbxAliquotaIRPF.TabIndex = 14;
+            // 
+            // mskbxSalarioFamilia
+            // 
+            this.mskbxSalarioFamilia.Location = new System.Drawing.Point(192, 342);
+            this.mskbxSalarioFamilia.Name = "mskbxSalarioFamilia";
+            this.mskbxSalarioFamilia.Size = new System.Drawing.Size(210, 26);
+            this.mskbxSalarioFamilia.TabIndex = 15;
+            // 
+            // mskbxSalarioLiquido
+            // 
+            this.mskbxSalarioLiquido.Location = new System.Drawing.Point(192, 388);
+            this.mskbxSalarioLiquido.Name = "mskbxSalarioLiquido";
+            this.mskbxSalarioLiquido.Size = new System.Drawing.Size(210, 26);
+            this.mskbxSalarioLiquido.TabIndex = 16;
+            // 
+            // mskbxDescontoINSS
+            // 
+            this.mskbxDescontoINSS.Location = new System.Drawing.Point(586, 240);
+            this.mskbxDescontoINSS.Name = "mskbxDescontoINSS";
+            this.mskbxDescontoINSS.Size = new System.Drawing.Size(152, 26);
+            this.mskbxDescontoINSS.TabIndex = 17;
+            // 
+            // mskbxDescontoIRPF
+            // 
+            this.mskbxDescontoIRPF.Location = new System.Drawing.Point(586, 296);
+            this.mskbxDescontoIRPF.Name = "mskbxDescontoIRPF";
+            this.mskbxDescontoIRPF.Size = new System.Drawing.Size(152, 26);
+            this.mskbxDescontoIRPF.TabIndex = 18;
+            // 
+            // Psalario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mskbxDescontoIRPF);
+            this.Controls.Add(this.mskbxDescontoINSS);
+            this.Controls.Add(this.mskbxSalarioLiquido);
+            this.Controls.Add(this.mskbxSalarioFamilia);
+            this.Controls.Add(this.mskbxAliquotaIRPF);
+            this.Controls.Add(this.mskbxAliquotaINSS);
+            this.Controls.Add(this.updwnNumeroFilhos);
+            this.Controls.Add(this.mskbxSalarioBruto);
+            this.Controls.Add(this.txtNomeFuncionario);
+            this.Controls.Add(this.lblDescontoIRPF);
+            this.Controls.Add(this.lblDescontoINSS);
+            this.Controls.Add(this.lblSalarioLiquido);
+            this.Controls.Add(this.lblSalarioFamilia);
+            this.Controls.Add(this.lblAliquotaIRPF);
+            this.Controls.Add(this.lblAliquotaINSS);
+            this.Controls.Add(this.btnVerificaDesconto);
+            this.Controls.Add(this.lblNumeroFilhos);
+            this.Controls.Add(this.lblSalarioBruto);
+            this.Controls.Add(this.lblNomeFuncionario);
+            this.Name = "Psalario";
+            this.Text = "Calcule Salário";
+            ((System.ComponentModel.ISupportInitialize)(this.updwnNumeroFilhos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNomeFuncionario;
+        private System.Windows.Forms.Label lblSalarioBruto;
+        private System.Windows.Forms.Label lblNumeroFilhos;
+        private System.Windows.Forms.Button btnVerificaDesconto;
+        private System.Windows.Forms.Label lblAliquotaINSS;
+        private System.Windows.Forms.Label lblAliquotaIRPF;
+        private System.Windows.Forms.Label lblSalarioFamilia;
+        private System.Windows.Forms.Label lblSalarioLiquido;
+        private System.Windows.Forms.Label lblDescontoINSS;
+        private System.Windows.Forms.Label lblDescontoIRPF;
+        private System.Windows.Forms.TextBox txtNomeFuncionario;
+        private System.Windows.Forms.MaskedTextBox mskbxSalarioBruto;
+        private System.Windows.Forms.NumericUpDown updwnNumeroFilhos;
+        private System.Windows.Forms.MaskedTextBox mskbxAliquotaINSS;
+        private System.Windows.Forms.MaskedTextBox mskbxAliquotaIRPF;
+        private System.Windows.Forms.MaskedTextBox mskbxSalarioFamilia;
+        private System.Windows.Forms.MaskedTextBox mskbxSalarioLiquido;
+        private System.Windows.Forms.MaskedTextBox mskbxDescontoINSS;
+        private System.Windows.Forms.MaskedTextBox mskbxDescontoIRPF;
+    }
+}
+
---
+++ Atividade5/Psalario/Psalario/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Psalario
+{
+    public partial class Psalario : Form
+    {
+        public Psalario()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}

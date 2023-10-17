9dd9d1b057ef96a52db6824ffc8ab6cc28c59347
ISABELLA PONTES <0030482321022@lti.fatecso.edu.br>
2023-10-17 10:55:42 -0300

    Add project files.

---
+++ Pmatrizes/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Pmatrizes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExercicio1 = new System.Windows.Forms.Button();
+            this.btnExercicio2 = new System.Windows.Forms.Button();
+            this.btnExercicio3 = new System.Windows.Forms.Button();
+            this.btnExercicio4 = new System.Windows.Forms.Button();
+            this.btnExercicio5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExercicio1
+            // 
+            this.btnExercicio1.Location = new System.Drawing.Point(90, 75);
+            this.btnExercicio1.Name = "btnExercicio1";
+            this.btnExercicio1.Size = new System.Drawing.Size(134, 62);
+            this.btnExercicio1.TabIndex = 0;
+            this.btnExercicio1.Text = "Exercício 1";
+            this.btnExercicio1.UseVisualStyleBackColor = true;
+            this.btnExercicio1.Click += new System.EventHandler(this.BtnExercicio1_Click);
+            // 
+            // btnExercicio2
+            // 
+            this.btnExercicio2.Location = new System.Drawing.Point(272, 75);
+            this.btnExercicio2.Name = "btnExercicio2";
+            this.btnExercicio2.Size = new System.Drawing.Size(138, 62);
+            this.btnExercicio2.TabIndex = 1;
+            this.btnExercicio2.Text = "Exercício 2";
+            this.btnExercicio2.UseVisualStyleBackColor = true;
+            // 
+            // btnExercicio3
+            // 
+            this.btnExercicio3.Location = new System.Drawing.Point(455, 75);
+            this.btnExercicio3.Name = "btnExercicio3";
+            this.btnExercicio3.Size = new System.Drawing.Size(132, 62);
+            this.btnExercicio3.TabIndex = 2;
+            this.btnExercicio3.Text = "Exercício 3";
+            this.btnExercicio3.UseVisualStyleBackColor = true;
+            // 
+            // btnExercicio4
+            // 
+            this.btnExercicio4.Location = new System.Drawing.Point(90, 187);
+            this.btnExercicio4.Name = "btnExercicio4";
+            this.btnExercicio4.Size = new System.Drawing.Size(134, 67);
+            this.btnExercicio4.TabIndex = 3;
+            this.btnExercicio4.Text = "Exercício 4";
+            this.btnExercicio4.UseVisualStyleBackColor = true;
+            // 
+            // btnExercicio5
+            // 
+            this.btnExercicio5.Location = new System.Drawing.Point(272, 187);
+            this.btnExercicio5.Name = "btnExercicio5";
+            this.btnExercicio5.Size = new System.Drawing.Size(138, 67);
+            this.btnExercicio5.TabIndex = 4;
+            this.btnExercicio5.Text = "Exercício 5";
+            this.btnExercicio5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExercicio5);
+            this.Controls.Add(this.btnExercicio4);
+            this.Controls.Add(this.btnExercicio3);
+            this.Controls.Add(this.btnExercicio2);
+            this.Controls.Add(this.btnExercicio1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExercicio1;
+        private System.Windows.Forms.Button btnExercicio2;
+        private System.Windows.Forms.Button btnExercicio3;
+        private System.Windows.Forms.Button btnExercicio4;
+        private System.Windows.Forms.Button btnExercicio5;
+    }
+}
+
